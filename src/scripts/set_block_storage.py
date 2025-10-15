2fa4b29d7f191f8218583b138866352eb77ef8c6
AlicePiemonti <alice.piemonti@mail.polimi.it>
2025-10-15 15:51:55 +0200

    fixed automatic creation of bucket

--- src/scripts/set_block_storage.py
+++ src/scripts/set_block_storage.py
@@ -1,6 +1,7 @@
 from prefect.filesystems import RemoteFileSystem
 from dotenv import load_dotenv
 import os
+import boto3
 
 # load env variables
 load_dotenv()
@@ -11,22 +12,30 @@ MINIO_DEFAULT_BUCKETS = os.getenv('MINIO_DEFAULT_BUCKETS')
 MINIO_HOST = os.getenv('MINIO_HOST')
 MINIO_PORT = os.getenv('MINIO_PORT')
 
-MINIO_SETTINGS = {
-    'key': MINIO_ROOT_USER,
-    'secret': MINIO_ROOT_PASSWORD,
-    'port': MINIO_PORT,
-    'host': MINIO_HOST,
-    'prefect-bucket': MINIO_DEFAULT_BUCKETS
-}
+# Connect to MinIO
+s3 = boto3.resource(
+    's3',
+    endpoint_url=f"http://{MINIO_HOST}:{MINIO_PORT}",
+    aws_access_key_id=MINIO_ROOT_USER,
+    aws_secret_access_key=MINIO_ROOT_PASSWORD,
+)
+
+# Create the bucket if it doesn't exist
+for bucket in MINIO_DEFAULT_BUCKETS.split(','):
+    bucket = bucket.strip()
+    if not s3.Bucket(bucket) in s3.buckets.all():
+        s3.create_bucket(Bucket=bucket)
+        print(f"Created bucket: {bucket}")
+
 
 minio_block = RemoteFileSystem(
-    basepath=f"s3://{MINIO_SETTINGS['prefect-bucket']}",
+    basepath=f"s3://{MINIO_DEFAULT_BUCKETS}",
     key_type="hash",
     settings=dict(
         use_ssl=False,
-        key=MINIO_SETTINGS['key'],
-        secret=MINIO_SETTINGS['secret'],
-        client_kwargs=dict(endpoint_url=f"http://{MINIO_SETTINGS['host']}:{MINIO_SETTINGS['port']}")
+        key=MINIO_ROOT_USER,
+        secret=MINIO_ROOT_PASSWORD,
+        client_kwargs=dict(endpoint_url=f"http://{MINIO_HOST}:{MINIO_PORT}")
     ),
 )
 minio_block.save("minio", overwrite=True)
