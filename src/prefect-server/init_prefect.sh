d17abbb7a5c56b3d7b7f4f3fdd7b473b74dd123d
AlicePiemonti <alice.piemonti@mail.polimi.it>
2025-11-19 10:20:59 +0100

    added metrics_ingestion pool

--- src/prefect-server/init_prefect.sh
+++ src/prefect-server/init_prefect.sh
@@ -6,3 +6,9 @@ prefect work-pool inspect LSTM_forecasting >/dev/null 2>&1 || \
 prefect work-pool create LSTM_forecasting --type process
 
 echo "Work pool 'LSTM_forecasting' is ready."
+
+# Create a work pool named metrics_ingestion if it doesn't exist
+prefect work-pool inspect metrics_ingestion >/dev/null 2>&1 || \
+prefect work-pool create metrics_ingestion --type process
+
+echo "Work pool 'metrics_ingestion' is ready."
