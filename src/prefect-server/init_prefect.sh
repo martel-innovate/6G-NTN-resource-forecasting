d236fcfe46482677fc405f5c5b372f0cb6ad9cc3
AlicePiemonti <alice.piemonti@mail.polimi.it>
2025-11-19 10:40:01 +0100

    added postgres_retention pool

--- src/prefect-server/init_prefect.sh
+++ src/prefect-server/init_prefect.sh
@@ -5,10 +5,18 @@ set -e
 prefect work-pool inspect LSTM_forecasting >/dev/null 2>&1 || \
 prefect work-pool create LSTM_forecasting --type process
 
-echo "Work pool 'LSTM_forecasting' is ready."
+# Work pool 'LSTM_forecasting' is ready.
 
 # Create a work pool named metrics_ingestion if it doesn't exist
 prefect work-pool inspect metrics_ingestion >/dev/null 2>&1 || \
 prefect work-pool create metrics_ingestion --type process
 
-echo "Work pool 'metrics_ingestion' is ready."
+# Work pool 'metrics_ingestion' is ready.
+
+# Create a work pool named postgres_retention if it doesn't exist
+prefect work-pool inspect postgres_retention >/dev/null 2>&1 || \
+prefect work-pool create postgres_retention --type process
+
+# Work pool 'postgres_retention' is ready.
+
+echo "Work pools 'LSTM_forecasting', 'metrics_ingestion' and 'postgres_retention' ready."
